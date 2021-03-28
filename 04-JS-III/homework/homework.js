c3fefb0447dd50710c7cf626aaf6d67f941c72ab
KailyKinG EDK <kailyking@gmail.com>
2021-03-28 00:39:29 -0300

    Termine Ejercicios 10 y 11 Del HomeWork JS _III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -113,7 +113,16 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var maximo= numeros[0];
+  for(var i = 1; i < numeros.length; i++){
+    if(numeros[i] > maximo){
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
 }
+numeroMasGrande([6,2,3,8,4]);
+
 
 
 function multiplicarArgumentos() {
